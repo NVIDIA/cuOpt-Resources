d6eed9899cbfbc323b7d78fd720dbe5da6703d52
rgsl888prabhu <ramakrishnap@nvidia.com>
2022-11-02 10:34:46 -0500

    update for 22.10.01

--- docker-utilities/utilities/run_server.sh
+++ docker-utilities/utilities/run_server.sh
@@ -21,7 +21,7 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 
-image=nvcr.io/nvidia/cuopt/cuopt:22.10
+image=nvcr.io/nvidia/cuopt/cuopt:22.10.01
 detached=False
 detached_log=
 
