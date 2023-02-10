42574df6ac5561e4b42ada8cd5b0de8aa55689af
Trevor McKay <tmgithub1@gmail.com>
2023-02-10 14:56:18 -0500

    Add flags to cuopt-helm.sh for setting namespace and nodeports (#38)
    This change makes it easier to deploy multiple instances of
    cuopt in kubernetes using the cuopt helm chart. The script
    takes arguments for namespace and nodeports so that additional
    instances of cuopt may be started on the same system.
    
    Default behavior of this script is unchanged.

--- cloud-scripts/scripts/cuopt-helm.sh
+++ cloud-scripts/scripts/cuopt-helm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# SPDX-FileCopyrightText: Copyright (c) 2022 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2022-2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: MIT
 
 # Permission is hereby granted, free of charge, to any person obtaining a
@@ -21,22 +21,80 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 
-NAMESPACE=cuopt-server
+# Call these out so we can reference them easily
+SERVER_DEFAULT=30000
+NOTEBOOK_DEFAULT=30001
+CHART_DEFAULT="cuopt-22.12.0.tgz"
+
+CUOPT_CHART=${CUOPT_CHART:-"$CHART_DEFAULT"}
+NAMESPACE=${NAMESPACE:-cuopt-server}
+NODE_PORT_SERVER=${NODE_PORT_SERVER:-"$SERVER_DEFAULT"}
+NODE_PORT_NOTEBOOK=${NODE_PORT_NOTEBOOK:-"$NOTEBOOK_DEFAULT"}
+SERVER_TYPE=${SERVER_TYPE:-"api"}
+
+function help {
+    echo "Convenience script for running the cuopt helm chart"
+    echo
+    echo "usage: cuopt-helm.sh [-h] [-n NAMESPACE] [-s NODE_PORT_SERVER]] [-j NODE_PORT_NOTEBOOK]] [-t SERVER_TYPE] [-c CUOPT_CHART]"
+    echo
+    echo "options:"
+    echo "  -n NAMESPACE          Deploy the helm chart in the specified namespace (default is cuopt-server)"
+    echo "  -s NODE_PORT_SERVER   The nodeport to use for the cuopt API server if deployed (default 30000)"
+    echo "  -j NODE_PORT_NOTEBOOK The nodeport to use for the cuopt Jupyter server if deployed (default 30001)"
+    echo "  -t SERVER_TYPE        The type of server to deploy, may be 'api', 'jupyter', or 'both' (default api)"
+    echo "  -c CUOPT_CHART        The cuopt helm chart to use (default $CHART_DEFAULT)"
+}
+
+while getopts "hn:s:j:t:c:" option; do
+   case $option in
+      h) # display Help
+         help
+         exit;;
+      n)
+         NAMESPACE=$OPTARG;;
+      s)
+         NODE_PORT_SERVER=$OPTARG;;
+      j)
+         NODE_PORT_NOTEBOOK=$OPTARG;;
+      t)
+         SERVER_TYPE=$OPTARG;;
+      c)
+	 CUOPT_CHART=$OPTARG;;
+     \?) # Invalid option
+         echo "Error: Invalid option"
+         exit;;
+   esac
+done
+
+echo "Chart is: $CUOPT_CHART"
+echo "Namespace is: $NAMESPACE"
+echo "Server type is: $SERVER_TYPE"
+echo "API server port: $NODE_PORT_SERVER"
+echo "Jupyter server port: $NODE_PORT_NOTEBOOK"
+
 kubectl create namespace $NAMESPACE
 
-helm fetch https://helm.ngc.nvidia.com/nvidia/cuopt/charts/cuopt-22.12.0.tgz --username='$oauthtoken' --password=$API_KEY --untar
+if [ ! -d "$CUOPT_CHART" ]; then
+    helm fetch https://helm.ngc.nvidia.com/nvstaging/cuopt/charts/$CUOPT_CHART --username='$oauthtoken' --password=$API_KEY --untar
+fi
+
+# Always set the port for each service, even if the service is not enabled.
+# In a future version of the chart, this will not be necessary if the
+# service is not enabled.
+server_str="--set node_port_server=$NODE_PORT_SERVER"
+notebook_str="--set node_port_notebook=$NODE_PORT_NOTEBOOK"
 
 case $SERVER_TYPE in
   "jupyter")
-    helm install --namespace $NAMESPACE --set ngc.apiKey=$API_KEY --set enable_nodeport=true nvidia-cuopt-chart cuopt --values cuopt/values_notebook.yaml
+    helm install --namespace $NAMESPACE --set ngc.apiKey=$API_KEY --set enable_nodeport=true $server_str $notebook_str nvidia-cuopt-chart cuopt --values cuopt/values_notebook.yaml
     ;;
 
   "both")
-    helm install --namespace $NAMESPACE --set ngc.apiKey=$API_KEY --set enable_nodeport=true --set enable_notebook_server=true nvidia-cuopt-chart cuopt --values cuopt/values.yaml
+    helm install --namespace $NAMESPACE --set ngc.apiKey=$API_KEY --set enable_nodeport=true --set enable_notebook_server=true $server_str $notebook_str nvidia-cuopt-chart cuopt --values cuopt/values.yaml
     ;;
 
   # default to api server in all other cases
   *)
-    helm install --namespace $NAMESPACE --set ngc.apiKey=$API_KEY --set enable_nodeport=true nvidia-cuopt-chart cuopt --values cuopt/values.yaml
+    helm install --namespace $NAMESPACE --set ngc.apiKey=$API_KEY --set enable_nodeport=true $server_str $notebook_str nvidia-cuopt-chart cuopt --values cuopt/values.yaml
     ;;
 esac
